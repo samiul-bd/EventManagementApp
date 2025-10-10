b41250f3a5b08dc9fe98fbc6c4cdc21e9fa83aa4
Samiul Alam <samiulalam.bd.ngn@.com>
2025-10-11 03:01:16 +0600

    New table created Users,Employees,Task

--- Database/DatabaseSchema.sql
+++ Database/DatabaseSchema.sql
@@ -7,10 +7,38 @@ go
 use EventManagementDB
 go
 
-Create Table Occasions (
+CREATE TABLE Occasions (
 Id INT PRIMARY KEY IDENTITY NOT NULL,
 OccasionName varchar (50) NOT NULL,
 OccasionDate date NOT NULL,
 [Description] varchar(100)
+)
+
+CREATE TABLE Users (
+Id int PRIMARY KEY IDENTITY NOT NULL,
+UserFName Varchar (20) NOT NULL,
+UserLName varchar (20) NOT NULL,
+Email Varchar (30),
+PhoneNumber varchar (20) NOT NULL,
+PresentAddress varchar (50) NOT NULL,
+PermanentAddress varchar (50)
+)
+
+CREATE TABLE Employees (
+Id int PRIMARY KEY IDENTITY NOT NULL,
+EmployeesFName varchar (20) NOT NULL,
+EmployeesLName varchar (20) NOT NULL,
+Email varchar (20) NOT NULL,
+PhoneNumber varchar (20) NOT NULL,
+PresentAddress varchar (50) NOT NULL,
+PermanentAddress varchar (50)
+)
+
+CREATE TABLE Task (
+Id INT PRIMARY KEY IDENTITY NOT NULL,
+StartTime varchar NOT NULL,
+EndTime varchar NOT NULL,
+[Weight] INT NOT NULL,
+[Status] BIT NOT NULL
 )
 
